346e520a876b3ba09e3daafe7b6828b3ee04b9bc
netcam22 <annettelesage@btinternet.com>
2023-11-12 16:13:29 +0000

    refactor: update to use async await

--- exercise001.js
+++ exercise001.js
@@ -1,20 +1,12 @@
-const promise = new Promise((resolve, reject) => {
-  // Generate random number between 0 and 9
-  const randomInt = Math.floor(Math.random() * 10);
-
+const getPromiseResponse = async (resolve, reject) => {
+  const randomInt = await Math.floor(Math.random() * 10);
   if (randomInt % 2 === 0) {
-    // Success
-    setTimeout(() => resolve("done"), 2000);
+    console.log(resolve);
   } else {
-    // Failure
-    setTimeout(() => reject("error"), 2000);
+    console.log(reject);
   }
-});
-
-promise
-  .then(data => {
-    console.log(`Yay! Promise resolved with response:${data}`);
-  })
-  .catch(error => {
-    console.log(`Boo. Promise rejected with response:${error}`);
-  });
+};
+getPromiseResponse(
+  `Yay! Promise resolved with response:done`,
+  `Boo. Promise rejected with response:error`
+);
