13c3c4d236b95292d02984ddf27f87706a19b9aa
netcam22 <annettelesage@btinternet.com>
2023-11-12 16:38:41 +0000

    refactor: implement async await

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,14 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+async function fetchData(apiEndPoint) {
+  const response = await fetch(apiEndPoint);
+  try {
+    const json = await response.json();
+    console.log(json);
+  } catch {
+    console.log(error);
+  }
+}
 
 fetchData(jsonTypicode);
